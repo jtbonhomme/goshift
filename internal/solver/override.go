a28510c52fc713ace217202f8f2124328e0f0546
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2024-03-27 18:29:07 +0100

    improve weekend statistics selection

--- internal/solver/override.go
+++ internal/solver/override.go
@@ -34,23 +34,23 @@ func (s *Solver) processOverride(label string, d time.Time, lastUsers []pagerdut
 			return override
 		}
 
+		log.Debug().Msgf("\t%s [%s] considering %s: %d | %d shifts (min Shifts: %d - min Weekends: %d | avg Shifts: %d - avg Weekends: %d)", label, d.String(), user.Email, s.Stats[user.Email], s.WeekendStats[user.Email], utils.Min(s.Stats), utils.Min(s.WeekendStats), utils.Average(s.Stats), utils.Average(s.WeekendStats))
+
 		// if user is un available that day, move to the next user
 		if slices.Contains(user.Unavailable, d) {
 			log.Debug().Msg(" not available this day --> NEXT")
 			continue
 		}
 
-		log.Debug().Msgf("\t%s [%s] considering %s: %d | %d shifts (avgShifts: %d - avgWeekends: %d)", label, d.String(), user.Email, s.Stats[user.Email], s.WeekendStats[user.Email], utils.Average(s.Stats), utils.Average(s.WeekendStats))
-
-		// already too much shifts for this user
-		//if s.Stats[n] > utils.Max(stats) || s.Stats[n] > utils.MinWithoutZero(stats)+1 || s.Stats[n] > utils.Average(stats)+1 {
-		if checkStats && s.Stats[user.Email] > utils.Average(s.Stats)+1 {
-			log.Debug().Msg(" stats too high --> NEXT")
+		// already too much weekend shifts for this user
+		if checkStats && weekday == time.Saturday.String() && s.WeekendStats[user.Email] > utils.Min(s.WeekendStats) {
+			log.Debug().Msg(" too much week-ends (> min) --> NEXT")
 			continue
 		}
 
-		if checkStats && weekday == time.Saturday.String() && s.WeekendStats[user.Email] > utils.Average(s.WeekendStats) {
-			log.Debug().Msg(" too much week-ends --> NEXT")
+		// already too much week days shifts for this user
+		if checkStats && weekday != time.Saturday.String() && checkStats && s.Stats[user.Email] > utils.Min(s.Stats) {
+			log.Debug().Msg(" stats too high (> min) --> NEXT")
 			continue
 		}
 
